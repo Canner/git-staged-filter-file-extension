518c7b558009af50f47b0e0c10316bee0d00583e
howardchi <howardchi@fougood.com>
2017-06-24 00:07:25 +0800

    initial start

--- src/index.ts
+++ src/index.ts
@@ -2,86 +2,104 @@
 import {Commit, ConvenientPatch, Diff, DiffFile, Oid, Repository, Tag, Tree} from "nodegit";
 import {resolve} from "path";
 
+type DiffTypes = Diff | Diff[];
+
+function noop() {} // tslint:disable-line
+
 export default class GitDiffCommit {
   private commitFrom: string;
+  private commitFromObj: Commit;
   private commitTo: string;
+  private commitToObj: Commit;
   private dirPath: string;
   private commitFromTree: Tree;
   private commitToTree: Tree;
 
   constructor(dirPath: string, commitFrom: string, commitTo: string) {
+    if (!commitFrom && commitTo) {
+      throw new Error("if commitTo is defined, commitFrom must defined");
+    }
     this.commitFrom = commitFrom;
     this.commitTo = commitTo;
     this.dirPath = resolve(process.cwd(), dirPath);
   }
 
-  public start(): Promise<ConvenientPatch[]> {
+  public start(): Promise<Diff[]> {
     return Repository.open(this.dirPath)
-      .then(this.getTagFromCommit)
-      .then(this.getTagToCommit)
+      .then(this.getCommitFromTree)
+      .then(this.getCommitToTree)
       .then(this.diffTreeToTree)
       .then(this.diff);
   }
 
-  public getTagFrom() {
-    return this.tagFrom;
+  public getCommitFrom() {
+    return this.commitFrom;
   }
 
-  public getTagTo() {
-    return this.tagTo;
+  public getCommitTo() {
+    return this.commitTo;
   }
 
-  private getTagToCommit = (repo: Repository): Promise<Repository> => {
-    const getTagTo = this.tagTo ? this.getTag(repo, this.tagTo) : repo.getHeadCommit();
-    return getTagTo
-            .then((commit) => {
-              return commit.getTree();
-            })
-            .then((tree) => {
-              this.headCommitTree = tree;
-              return repo;
-            });
-  }
-
-  private getTagFromCommit = (repo: Repository): Promise<Repository> => {
-    const getTagFrom = this.getTag(repo, this.tagFrom);
-    return getTagFrom
-      .then((commit) => {
-        return commit.getTree();
-      })
-      .then((tree) => {
-        this.tagCommitTree = tree;
-        return repo;
-      });
-  }
-
-  private getTag(repo: Repository, getTagShort: string): Promise<Commit> {
-    return Tag.list(repo).then((arr) => {
-          if (!getTagShort) {
-            getTagShort = arr[arr.length - 1];
-            this.tagFrom = getTagShort;
-          }
-
-          // check if tags are exist.
-          if (arr.indexOf(getTagShort) === -1) {
-            // not found tag
-            throw new Error(`Make sure your tag is in the right name, can't find tag: ${getTagShort}`);
-          }
-          return repo.getTagByName(getTagShort);
+  private getCommitToTree = (repo: Repository): Promise<Repository> => {
+    if (this.commitTo) {
+      const getCommitTo = this.getCommit(repo, this.commitTo);
+      return getCommitTo
+        .then((commit) => {
+          this.commitToObj = commit;
+          return commit.getTree();
         })
-        .then((tag) => {
-          return repo.getCommit(tag.targetId());
-        })
-        .catch((e) => {
-          return repo.getReferenceCommit(getTagShort);
+        .then((tree) => {
+          this.commitToTree = tree;
+          return repo;
         });
+    }
+
+    return Promise.resolve(repo);
   }
 
-  private diffTreeToTree = (repo: Repository): Promise<Diff> => {
-    return Diff.treeToTree(repo, this.tagCommitTree, this.headCommitTree, null);
+  private getCommitFromTree = (repo: Repository): Promise<Repository> => {
+    if (this.commitFrom) {
+      const getCommitFrom = this.getCommit(repo, this.commitFrom);
+      return getCommitFrom
+        .then((commit) => {
+          this.commitFromObj = commit;
+          return commit.getTree();
+        })
+        .then((tree) => {
+          this.commitFromTree = tree;
+          return repo;
+        });
+    }
+
+    return Promise.resolve(repo);
   }
 
-  private diff = (diff: Diff): Promise<ConvenientPatch[]> => {
-    return diff.patches();
+  private getCommit(repo: Repository, commitSha: string): Promise<Commit> {
+    const oid = Oid.fromString(commitSha);
+    return Commit.lookupPrefix(repo, oid, commitSha.length);
+  }
+
+  private diffTreeToTree = (repo: Repository): Promise<DiffTypes> => {
+    if (!this.commitFrom) {
+      // if commitFrom is not defined get from HEAD commit to current
+      return repo.getHeadCommit()
+        .then((commit) => {
+          return commit.getDiff(noop);
+        });
+    }
+
+    if (!this.commitTo) {
+      // if commitTo is not defined get from commitFrom to current
+      return this.commitFromObj.getDiff(noop);
+    }
+    return Diff.treeToTree(repo, this.commitFromTree, this.commitToTree, null);
+  }
+
+  private diff = (diff: DiffTypes): Promise<Diff[]> => {
+    if (Array.isArray(diff)) {
+      return Promise.resolve(diff);
+    }
+
+    return Promise.resolve([diff]);
   }
 }
--- test/index-spec.ts
+++ test/index-spec.ts
@@ -1,88 +1,19 @@
 import {expect} from "chai";
-import {DiffFile, Repository, Tag} from "nodegit";
-import GitDiffTags from "../src/index";
+import {ConvenientPatch, DiffFile, Repository, Tag} from "nodegit";
+import GitDiffCommits from "../src/index";
 
-describe("diff files between tags", () => {
+describe("diff files between commits", () => {
   it("should get modified package.json", (done) => {
-    const diff = new GitDiffTags("./", "v0.2.0", "v0.2.0-a");
+    const diff = new GitDiffCommits("./", "1fb5a328806fa8dd8", null);
 
     diff.start()
       .then((result) => {
-        expect(result[0].isAdded()).to.be.false; // tslint:disable-line
-        expect(result[0].isDeleted()).to.be.false; // tslint:disable-line
-        expect(result[0].isModified()).to.be.true; // tslint:disable-line
-        expect(result[0].newFile().path()).eql("package.json"); // tslint:disable-line
-        done();
+        return result[0].patches();
       })
-      .catch((err) => {
-        done(new Error(err));
-      });
-  });
-
-  it("should get created file test/test-new-file.js", (done) => {
-    const diff = new GitDiffTags("./", "v0.2.0-a", "v0.2.0-b");
-
-    diff.start()
-      .then((result) => {
-        expect(result[0].isAdded()).to.be.true; // tslint:disable-line
-        expect(result[0].isDeleted()).to.be.false; // tslint:disable-line
-        expect(result[0].isModified()).to.be.false; // tslint:disable-line
-        expect(result[0].newFile().path()).eql("test/test-new-file.js"); // tslint:disable-line
-        done();
-      })
-      .catch((err) => {
-        done(new Error(err));
-      });
-  });
-
-  it("should delete file test/test-new-file.js", (done) => {
-    const diff = new GitDiffTags("./", "v0.2.0-b", "v0.2.0-c");
-
-    diff.start()
-      .then((result) => {
-        expect(result[0].isAdded()).to.be.false; // tslint:disable-line
-        expect(result[0].isDeleted()).to.be.true; // tslint:disable-line
-        expect(result[0].isModified()).to.be.false; // tslint:disable-line
-        expect(result[0].newFile().path()).eql("test/test-new-file.js"); // tslint:disable-line
-        done();
-      })
-      .catch((err) => {
-        done(new Error(err));
-      });
-  });
-
-  it("should diff through three version tags", (done) => {
-    const diff = new GitDiffTags("./", "v0.2.0", "v0.2.0-c");
-
-    diff.start()
-      .then((result) => {
-        expect(result.length).equals(1);
-        expect(result[0].isAdded()).to.be.false; // tslint:disable-line
-        expect(result[0].isDeleted()).to.be.false; // tslint:disable-line
-        expect(result[0].isModified()).to.be.true; // tslint:disable-line
-        expect(result[0].newFile().path()).eql("package.json"); // tslint:disable-line
-        done();
-      })
-      .catch((err) => {
-        done(new Error(err));
-      });
-  });
-
-  it("get last tag if tagFrom is null", (done) => {
-    const diff = new GitDiffTags("./", null, null);
-
-    diff.start()
-      .then((result) => {
-        return Repository.open("./")
-          .then((repo) => {
-            return Tag.list(repo).then((arr) => {
-              return arr[arr.length - 1];
-            });
-          });
-      })
-      .then((lastTag) => {
-        expect(diff.getTagFrom()).equals(lastTag);
-
+      .then((arrayConvenientPatch: ConvenientPatch[]) => {
+        arrayConvenientPatch.forEach((patch) => {
+          console.log(patch.newFile().path());
+        });
         done();
       })
       .catch((err) => {
