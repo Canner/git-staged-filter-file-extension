1fb5a328806fa8dd8f27fb17297852118588fa90
howardchi <howardchi@fougood.com>
2017-06-23 23:08:33 +0800

    init

---
+++ src/index.ts
@@ -0,0 +1,87 @@
+
+import {Commit, ConvenientPatch, Diff, DiffFile, Oid, Repository, Tag, Tree} from "nodegit";
+import {resolve} from "path";
+
+export default class GitDiffCommit {
+  private commitFrom: string;
+  private commitTo: string;
+  private dirPath: string;
+  private commitFromTree: Tree;
+  private commitToTree: Tree;
+
+  constructor(dirPath: string, commitFrom: string, commitTo: string) {
+    this.commitFrom = commitFrom;
+    this.commitTo = commitTo;
+    this.dirPath = resolve(process.cwd(), dirPath);
+  }
+
+  public start(): Promise<ConvenientPatch[]> {
+    return Repository.open(this.dirPath)
+      .then(this.getTagFromCommit)
+      .then(this.getTagToCommit)
+      .then(this.diffTreeToTree)
+      .then(this.diff);
+  }
+
+  public getTagFrom() {
+    return this.tagFrom;
+  }
+
+  public getTagTo() {
+    return this.tagTo;
+  }
+
+  private getTagToCommit = (repo: Repository): Promise<Repository> => {
+    const getTagTo = this.tagTo ? this.getTag(repo, this.tagTo) : repo.getHeadCommit();
+    return getTagTo
+            .then((commit) => {
+              return commit.getTree();
+            })
+            .then((tree) => {
+              this.headCommitTree = tree;
+              return repo;
+            });
+  }
+
+  private getTagFromCommit = (repo: Repository): Promise<Repository> => {
+    const getTagFrom = this.getTag(repo, this.tagFrom);
+    return getTagFrom
+      .then((commit) => {
+        return commit.getTree();
+      })
+      .then((tree) => {
+        this.tagCommitTree = tree;
+        return repo;
+      });
+  }
+
+  private getTag(repo: Repository, getTagShort: string): Promise<Commit> {
+    return Tag.list(repo).then((arr) => {
+          if (!getTagShort) {
+            getTagShort = arr[arr.length - 1];
+            this.tagFrom = getTagShort;
+          }
+
+          // check if tags are exist.
+          if (arr.indexOf(getTagShort) === -1) {
+            // not found tag
+            throw new Error(`Make sure your tag is in the right name, can't find tag: ${getTagShort}`);
+          }
+          return repo.getTagByName(getTagShort);
+        })
+        .then((tag) => {
+          return repo.getCommit(tag.targetId());
+        })
+        .catch((e) => {
+          return repo.getReferenceCommit(getTagShort);
+        });
+  }
+
+  private diffTreeToTree = (repo: Repository): Promise<Diff> => {
+    return Diff.treeToTree(repo, this.tagCommitTree, this.headCommitTree, null);
+  }
+
+  private diff = (diff: Diff): Promise<ConvenientPatch[]> => {
+    return diff.patches();
+  }
+}
